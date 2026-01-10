332be05fa5e6ea66c3423e6ae59bab7b153cdcc9
urknall <ur-knall@gmx.net>
2026-01-10 17:24:57 +0100

    Update release.pl

--- release.pl
+++ release.pl
@@ -4,11 +4,11 @@
 #   version:  Version string (e.g., "1.0.0")
 #   zipfile:  Path to zip file to calculate SHA for
 #   url:      Base URL (zip filename will be appended)
-#   name:     Applet name identifier (e.g., "CustomClockHelper")
+#   name:     Plugin name identifier (e.g., "CustomClockHelper")
 #
 # Examples:
 #   perl release.pl repo.xml 1.0.0 CustomClockHelper.zip http://example.com CustomClockHelper
-#   perl release.pl repo.xml 2.0.0 JLJiveApplet.zip http://example.com JLJiveApplet
+#   perl release.pl repo.xml 2.0.0 PluginName.zip http://example.com PluginName
 
 use strict;
 
@@ -24,33 +24,33 @@ my $name = $ARGV[4];
 
 # Name parameter is required
 if (!defined $name || $name eq '') {
-    die "Error: Applet name parameter is required\n";
+    die "Error: Plugin name parameter is required\n";
 }
 
 my $repo = XMLin($repofile, ForceArray => 1, KeepRoot => 0, KeyAttr => 0, NoAttr => 0);
 
-# Ensure applets section exists
-if (!exists $repo->{applets}) {
-    $repo->{applets} = [{ applet => [] }];
-} elsif (!exists $repo->{applets}[0]->{applet}) {
-    $repo->{applets}[0]->{applet} = [];
+# Ensure plugins section exists
+if (!exists $repo->{plugins}) {
+    $repo->{plugins} = [{ plugin => [] }];
+} elsif (!exists $repo->{plugins}[0]->{plugin}) {
+    $repo->{plugins}[0]->{plugin} = [];
 }
 
-# Find the applet with matching name, or create new one
-my $applet_index = -1;
-my $applets = $repo->{applets}[0]->{applet};
+# Find the plugin with matching name, or create new one
+my $plugin_index = -1;
+my $plugins = $repo->{plugins}[0]->{plugin};
 
-for (my $i = 0; $i < scalar(@$applets); $i++) {
-    if (exists $applets->[$i]->{name} && $applets->[$i]->{name} eq $name) {
-        $applet_index = $i;
+for (my $i = 0; $i < scalar(@$plugins); $i++) {
+    if (exists $plugins->[$i]->{name} && $plugins->[$i]->{name} eq $name) {
+        $plugin_index = $i;
         last;
     }
 }
 
-# If not found, add new applet entry
-if ($applet_index == -1) {
-    push @$applets, { name => $name };
-    $applet_index = scalar(@$applets) - 1;
+# If not found, add new plugin entry
+if ($plugin_index == -1) {
+    push @$plugins, { name => $name };
+    $plugin_index = scalar(@$plugins) - 1;
 }
 
 # Calculate SHA
@@ -61,12 +61,12 @@ my $digest = Digest::SHA->new;
 $digest->addfile($fh);
 close $fh;
 
-# Update the applet entry
-$applets->[$applet_index]->{name} = $name;
-$applets->[$applet_index]->{version} = $version;
-$applets->[$applet_index]->{sha}[0] = $digest->hexdigest;
+# Update the plugin entry
+$plugins->[$plugin_index]->{name} = $name;
+$plugins->[$plugin_index]->{version} = $version;
+$plugins->[$plugin_index]->{sha}[0] = $digest->hexdigest;
 $url .= "/$zipfile";
-$applets->[$applet_index]->{url}[0] = $url;
+$plugins->[$plugin_index]->{url}[0] = $url;
 
 print("name   : $name\n");
 print("version: $version\n");
